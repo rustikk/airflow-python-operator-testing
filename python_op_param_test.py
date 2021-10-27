ffade93a9737a947897786d877f192094ffba60c
tunruh03 <tunruh01@gmail.com>
2021-10-26 18:07:55 -0600

    checking templates_dict and trying templates_ext

--- python_op_param_test.py
+++ python_op_param_test.py
@@ -3,44 +3,44 @@ from airflow.operators.python_operator import PythonOperator
 
 from datetime import timedelta, datetime
 
+
 def kw_parameter_check(**kwargs):
     kw = kwargs
     return kw
+
 
 def ar_parameter_check(*data):
     ar = data
     return ar
 
+
 default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'email_on_failure': False,
-    'retries': 3,
-    'retry_delay': timedelta(minutes=5)
+    "owner": "airflow",
 }
 
 with DAG(
     dag_id="python_operator_tests",
+    owner="airflow",
     start_date=datetime(2021, 10, 24),
     schedule_interval=timedelta(minutes=10),
     catchup=False,
     default_args=default_args,
-    tags=["Python_Operator_Test"]
+    tags=["Python_Operator_Test"],
 ) as dag:
-    #this says it cant deserialize json
-    #error code:
-    #TypeError: Object of type AirflowConfigParser is not JSON serializable
+    # this says it cant deserialize json
+    # error code:
+    # TypeError: Object of type AirflowConfigParser is not JSON serializable
     t1 = PythonOperator(
         task_id="parameter_test",
         python_callable=kw_parameter_check,
-        op_kwargs={"kwarg1": "kwarg_one", "kwarg2": "kwarg_two"}
+        op_kwargs={"kwarg1": "kwarg_one", "kwarg2": "kwarg_two"},
     )
-    
-    #this works
+
+    # this works
     t2 = PythonOperator(
         task_id="args_test",
         python_callable=ar_parameter_check,
-        op_args=["Hello", "QA", "Team", "!"]
+        op_args=["Hello", "QA", "Team", "!"],
     )
 
 
---
+++ python_op_param_test_v2.py
@@ -0,0 +1,62 @@
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+
+from datetime import timedelta, datetime
+
+from pprint import pprint
+
+import sys
+
+yesterday = datetime.now() - timedelta(days=1)
+
+
+def kw_parameter_check(**kwargs):
+    pprint(kwargs)
+
+
+def ar_parameter_check(*data):
+    pprint(data)
+
+
+def templated_dict_check(templates_dict):
+    # pprint(templates_dict)
+    return templates_dict
+
+
+with DAG(
+    dag_id="python_operator_tests",
+    start_date=yesterday,
+    schedule_interval=None,
+    catchup=False,
+    template_searchpath="/home/t-dawg/Documents/astronomer_tut/templates",
+    tags=["Python_Operator_Test"],
+) as dag:
+
+    t1 = PythonOperator(
+        task_id="kwargs_test",
+        python_callable=kw_parameter_check,
+        op_kwargs={"kwarg1": "kwarg_one", "kwarg2": "kwarg_two"},
+    )
+
+    t2 = PythonOperator(
+        task_id="args_test",
+        python_callable=ar_parameter_check,
+        op_args=["Hello", "QA", "Team", "!"],
+    )
+
+    # shows as both a xcom with a value of return value or
+    # can be seen in the rendered Template tab next to XComs
+    t3 = PythonOperator(
+        task_id="template_dict_test",
+        python_callable=templated_dict_check,
+        # converts key values to str in xcoms
+        # templates_dict={"Hello": "There", 12: 14, 14: "string"},
+        templates_dict={
+            "sql": "sqller.sql",
+            "website_boilerplt": "texty.html",
+            "python": "pypy.py",
+        },
+        # templates_exts=(".sql", ".html", ".py"),
+    )
+
+t1 >> t2 >> t3
